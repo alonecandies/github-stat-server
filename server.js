4165a5eb47eeae63c42854b384ae15cd95ec58d7
alonecandies <dragonfrezze@gmail.com>
2022-05-24 17:24:43 +0700

    vercel

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
